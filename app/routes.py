03df0029f09696c348701cb1662ebae2ef94e855
James Price <james.joseph.price@gmail.com>
2019-07-03 16:14:46 -0400

    change url to live Huge webhook

--- app/routes.py
+++ app/routes.py
@@ -14,21 +14,21 @@ def index():
 		<title>GummyTest</title>
 	</head>
 	<body>
-		<a href=/yes><input type=”button” value=”There are Gummy Worms”></a>
+		<a href=/yes><input type="button" value="There are Gummy Worms"></a>
 		<a href=/no><input type="button" value="There are NO Gummy Worms"></a>
 	</body>
 </html>'''
 
 
-payload1 = {"text":"Yes Test"})
+payload1 = {"text":"Yes Test"}
 payload2 = {"text":"No Test"}
 
 @app.route('/yes')
 def yes(): 
-	requests.post('https://hooks.slack.com/services/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', json=payload1)
+	requests.post('https://hooks.slack.com/services/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', json=payload1)
 	return redirect(url_for('index')) 
 
 @app.route('/no')
 def no(): 
-	requests.post('https://hooks.slack.com/services/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', json=payload2)
+	requests.post('https://hooks.slack.com/services/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', json=payload2)
 	return redirect(url_for('index')) 
